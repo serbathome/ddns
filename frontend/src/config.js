eb9c4afe848f737acd5538ac7011253d0a1c0df7
Sergey Biryukov <sbiryukov@microsoft.com>
2026-01-01 19:01:29 +0100

    Update apiUrl in config to use the correct production endpoint

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export const config = {
   domainName: import.meta.env.VITE_DOMAIN_NAME || 'my-cloud-lab.com',
-  apiUrl: import.meta.env.VITE_API_URL || 'http://backend:8080'
+  apiUrl: import.meta.env.VITE_API_URL || 'https://backend.icyforest-8d63be1c.germanywestcentral.azurecontainerapps.io'
 };
